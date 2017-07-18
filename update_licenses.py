e2f16e232bc1e5dc9d85ca383b9eb8d2ab0c2254
Craig Northway <craig.northway@gmail.com>
2017-07-18 12:49:37 -0700

    Change local variables to snake case

--- update_licenses.py
+++ update_licenses.py
@@ -27,17 +27,17 @@ def license_text_to_str(literal):
     # Assume the literal contains a list of <p> elements under a top level node.
     return xml_to_str(literal.value.firstChild) + '\n'
 
-def get_license_text(licenseId):
-    return get_license_term(licenseId, "http://spdx.org/rdf/terms#licenseText")
+def get_license_text(license_id):
+    return get_license_term(license_id, "http://spdx.org/rdf/terms#licenseText")
 
-def get_license_header(licenseId):
-    return get_license_term(licenseId, "http://spdx.org/rdf/terms#standardLicenseHeader")
+def get_license_header(license_id):
+    return get_license_term(license_id, "http://spdx.org/rdf/terms#standardLicenseHeader")
 
-def get_license_term(licenseId, term):
+def get_license_term(license_id, term):
     graph = rdflib.Graph()
 
     try:
-        graph.parse('http://spdx.org/licenses/' + licenseId + '.html')
+        graph.parse('http://spdx.org/licenses/' + license_id + '.html')
     except:
         return None
 
@@ -46,30 +46,30 @@ def get_license_term(licenseId, term):
     return license_text_to_str(objs.next()[1])
 
 def write_licenses_dir(ids):
-    licenseDir = 'codebom/licenses'
-    if not os.path.exists(licenseDir):
-        os.makedirs(licenseDir)
+    license_dir = 'codebom/licenses'
+    if not os.path.exists(license_dir):
+        os.makedirs(license_dir)
 
-    for licenseId in ids:
-        sys.stdout.write("Updating license text for '{}'\n".format(licenseId))
-        text = get_license_text(licenseId)
+    for license_id in ids:
+        sys.stdout.write("Updating license text for '{}'\n".format(license_id))
+        text = get_license_text(license_id)
         if text is None:
             continue
         
-        write_license_text(licenseDir, licenseId, text)
+        write_license_text(license_dir, license_id, text)
         
-        header = get_license_header(licenseId)
+        header = get_license_header(license_id)
         if header is not None:
-            write_license_header(licenseDir, licenseId, header)
+            write_license_header(license_dir, license_id, header)
 
-def write_license_text(licenseDir, licenseId, text):
-    write_content(licenseDir, licenseId, "", text)
+def write_license_text(license_dir, license_id, text):
+    write_content(license_dir, license_id, "", text)
 
-def write_license_header(licenseDir, licenseId, header):
-    write_content(licenseDir, licenseId, "-header", header)
+def write_license_header(license_dir, license_id, header):
+    write_content(license_dir, license_id, "-header", header)
 
-def write_content(licenseDir, licenseId, suffix, text):
-        with open('{}/{}{}.txt'.format(licenseDir, licenseId, suffix), 'w') as hdl:
+def write_content(license_dir, license_id, suffix, text):
+        with open('{}/{}{}.txt'.format(license_dir, license_id, suffix), 'w') as hdl:
             hdl.write(text)
 
 with open('codebom/licenses.py', 'w') as out:
